f8a6401859561b6cf84e351c2f6ecdf6aeae4b85
sagar-masale <sagarmasale27@gmail.com>
2026-04-14 00:11:19 +0530

    fix bug

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/",   // ❌ /Wedding/ हटाओ, सिर्फ "/" रखो
-})
+
+export default defineConfig(({ mode }) => ({
+  base: '/',
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
